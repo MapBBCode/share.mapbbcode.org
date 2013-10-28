e43f3267b3bd73c088cc04727f01d21af27a1e94
Ilya Zverev <zverik@textual.ru>
2013-10-28 22:53:06 +0400

    md5 identifiers for stronger security

--- auth.php
+++ auth.php
@@ -41,6 +41,7 @@ try {
         </noscript>
         <input type="button" value="No, thanks" onclick="window.close();">
     </form>
+    <div style="margin-top: 2em;">A note on privacy: only a hash of your identifier is stored in the database, which means no one, not even an administrator, can trace it back to you. The hash is used for storing your library of maps, but no maps can be linked to you personally, even by a person with database access. Only those who have access to your account (or your computer while you are logged in) can see your map library.</div>
     <script>openid.init('openid_identifier');</script>
 </body>
 </html>
@@ -56,7 +57,7 @@ try {
         ini_set('session.cookie_lifetime', 7776000);
         session_set_cookie_params(7776000);
         session_start();
-        $_SESSION['user_id'] = $id;
+        $_SESSION['user_id'] = md5($id);
         $message = 'You have logged in as '.$id;
         $close = true;
     }
