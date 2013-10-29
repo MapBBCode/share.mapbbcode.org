08300cdcb802981a109c3daabbe3ce2fa91c9ac9
Ilya Zverev <zverik@textual.ru>
2013-10-30 00:02:40 +0400

    removed footer into a separate file

--- page.php
+++ page.php
@@ -410,22 +410,6 @@ function openCodeEditor(edit) {
 }
 
 </script>
-
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["setDomains", ["*.mapbbcode.org","*.share.mapbbcode.org"]]);
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.textual.ru/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "6"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
+<?php @include('footer.php'); ?>
 </body>
 </html>
