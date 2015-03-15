44b03c2448d298e2035bb44e594bb5ea0a601301
Ilya Zverev <zverik@textual.ru>
2015-03-15 12:51:52 +0300

    do not break gpx areas

--- formats/gpx.php
+++ formats/gpx.php
@@ -106,8 +106,8 @@ class GPXFormat implements Format {
                     if( $mode == 'wpt' || $mode == 'trkseg' || $mode == 'rte' || $mode == 'trk' ) {
 						if( isset($coords) && count($coords) > 0 ) {
 							$cnt = count($coords);
-							if( count($coords) > 2 && $coords[0][0] == $coords[$cnt-1][0] && $coords[0][1] == $coords[$cnt-1][1] )
-								array_pop($coords);
+							//if( count($coords) > 2 && $coords[0][0] == $coords[$cnt-1][0] && $coords[0][1] == $coords[$cnt-1][1] )
+							//	array_pop($coords);
                             $obj = array('coords' => $coords);
                             if( isset($objtext) )
                                 $obj['text'] = $objtext;
