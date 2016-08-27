f594043ecd6c117a11dc183db7eae0581d57b106
Acer4830\Sergio <s.glazomitsky@gmail.com>
2016-08-27 18:03:03 +0300

    generates IFRAME code (just plain text)

---
+++ formats/iframelink.php
@@ -0,0 +1,33 @@
+<?php
+
+class IFrameLinkFormat implements Format {
+    public $title = 'IFRAME';
+    public $mime = 'text/html';
+    public $can_attach = false;
+
+    public function export( $data ) {
+        global $scodeid, $base_path;
+
+        $s = isset($scodeid) ? 
+            '<iframe src="'.$base_path.'/'.$scodeid.'?format=iframe&direct' :
+            '<iframe src="'.$base_path.'/?format=iframe&direct&bbcode='.array_to_mapbbcode($data);
+        $f = '" width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
+        $iframe = $s.$f;
+
+        $html = 
+'<!DOCTYPE html><html><head><title>IFRAME export</title><meta charset="utf-8"></head><body>'.
+'<p>If you would like to link to this route, just copy the code and past it into your source code:</p>'.
+'<p><code>'.htmlspecialchars($iframe).'</code></p>'.
+'</body></html>';
+        return $html;
+    }
+
+    public function test( $header ) {
+        return false;
+    }
+
+    public function import( $file ) {
+    }
+}
+
+$formats['iframelink'] = new IFrameLinkFormat();
